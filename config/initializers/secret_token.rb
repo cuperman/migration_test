f479e92cd14ec74faf5beb816e06761b52b0823a
Jeff Cooper <jcooper@wikia-inc.com>
2014-01-14 02:33:40 +0000

    New Rails 3.2.16 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MigrationTest::Application.config.secret_token = '0949aedc5b360545fb3a25914aa74c7d13a4939f67777df36db4c52eb5f74f6c52b6b8d9ff59d59f9247f165d0f6a78761234bbdd11df8770799c199adb790a6'
